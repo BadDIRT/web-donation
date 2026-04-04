ca2d716c6aeffce549fff07004a3b72b9e7d3c4d
BadDIRT <baddirtkarat@gmail.com>
2026-04-04 10:56:16 +0700

    menambah logo pada semua title di layouts

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
+    <link rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><rect width='24' height='24' rx='7' fill='%2322C55E'/><g transform='translate(12,12) scale(0.75) translate(-12,-12)'><path fill='none' stroke='%23FFFFFF' stroke-width='2.5' stroke-linecap='round' stroke-linejoin='round' d='M12 21s-6.5-4.35-9-8.5C1.5 9.5 3.5 6 7 6c2 0 3.5 1.5 5 3.5C13.5 7.5 15 6 17 6c3.5 0 5.5 3.5 4 6.5-2.5 4.15-9 8.5-9 8.5z'/></g></svg>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     @vite('resources/css/app.css')
@@ -16,7 +18,7 @@
 
 <body class="min-h-screen bg-gradient-to-br from-green-50 to-white flex items-center justify-center">
 
-    
+
 
     {{-- BACK BUTTON --}}
     <a href="{{ url()->previous() ?? '/' }}"
